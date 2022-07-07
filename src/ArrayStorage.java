2c1d742b0198ea250f4c1e8cc8a96d481f7ba635
Umed Davlatov <umedjon721@gmail.com>
2022-07-07 17:47:27 +0300

    написал два метода, еще один надо доделать дома!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,28 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int k = 0;
 
     void clear() {
+        storage = null;
     }
 
     void save(Resume r) {
+
+        for (int i = 0; i <= k; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+            }
+        }
+        k++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                count = i;
+                break;
+            }
+        }
+        Resume[] arrNewStorage = new Resume[storage.length - 1];
+        int remainingElements = storage.length - (count + 1);
+        System.arraycopy(storage, 0, arrNewStorage, 0, count);
+        System.arraycopy(storage, count + 1, arrNewStorage, count, remainingElements);
+        //System.out.println("Elements -- "  + Arrays.toString(arrDestination));
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        for (int i = 0; i < k; i++) {
+            return new Resume[i];
+        }
+        return null;
     }
+
 
     int size() {
         return storage.length;
--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -16,10 +16,14 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
+        System.out.println(ARRAY_STORAGE.storage[0].uuid);
+        System.out.println(ARRAY_STORAGE.storage[1].uuid);
+        System.out.println(ARRAY_STORAGE.storage[2].toString());
+
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+        // System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
         ARRAY_STORAGE.delete(r1.uuid);
